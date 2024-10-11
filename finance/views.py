b327830bf9a9b062766214966a0c10add08ca407
Prajwal <pg8253876@gmail.com>
2024-10-11 12:15:01 +0530

    hi sandy

--- finance/views.py
+++ finance/views.py
@@ -2,4 +2,5 @@
 #new change made by prajwal
 from django.shortcuts import render
 def hello(request):
-    return render('<h1>Hello welcome to my django project</h1>')
+    return render('<h1>Hello welcome to our django project</h1>')
+
